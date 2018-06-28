faaac2b9588f02c4830422fdf93773b8bd78ee31
michel zarpelon <michelzarpe@gmail.com>
2018-06-28 09:01:53 -0300

    Capitulo 1 - Saída de dados

--- capitulo 1/TesteSDK/TesteSDK/Program.cs
+++ capitulo 1/TesteSDK/TesteSDK/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
-namespace TesteSDK
+namespace Aplication
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            string nome;
+            string nome = "";
+            double x = 10.35784;
+            int y = 32;
+            string z = "Maria";
+            char w = 'F';
+
+
+
             Console.WriteLine("digite seu nome");
             nome = Console.ReadLine();
+            Console.WriteLine("Variável X ->"+x);
+            Console.WriteLine("Variável X com F2 ->" + x.ToString("F2"));
+            Console.WriteLine("Variável X com F4 ->" + x.ToString("F4"));
+            Console.WriteLine("Variável X com F2 ->" + x.ToString("F2"),CultureInfo.InvariantCulture);
+         
 
-            Console.WriteLine("Seu nome é:"+nome);
+
+
             Console.ReadLine();
+
+
+
+
+
 
 
         }
