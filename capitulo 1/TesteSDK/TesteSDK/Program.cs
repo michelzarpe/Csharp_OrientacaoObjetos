99256b8cd4167cd7bae558a996dab1f3ff1b4690
michel zarpelon <michelzarpe@gmail.com>
2018-06-28 09:18:42 -0300

    Capitulo 1 - Entrada de Dados

--- capitulo 1/TesteSDK/TesteSDK/Program.cs
+++ capitulo 1/TesteSDK/TesteSDK/Program.cs
@@ -17,11 +17,28 @@ namespace Aplication
 
             Console.WriteLine("digite seu nome");
             nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu salário Atual");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("R ou U");
+            w = char.Parse(Console.ReadLine());
+
             Console.WriteLine("Variável X ->"+x);
             Console.WriteLine("Variável X com F2 ->" + x.ToString("F2"));
             Console.WriteLine("Variável X com F4 ->" + x.ToString("F4"));
-            Console.WriteLine("Variável X com F2 ->" + x.ToString("F2"),CultureInfo.InvariantCulture);
-         
+            Console.WriteLine("Variável X com F2 ->" + x.ToString("F2", CultureInfo.InvariantCulture)+" "+ w+"$");
+
+
+            /*ler uma frase*/
+            string[] frase;
+            frase = Console.ReadLine().Split(' ');
+
+            for(int l = 0; l< frase.Length; l++)
+            {
+                Console.WriteLine(frase[l]);
+
+            }
 
 
 
