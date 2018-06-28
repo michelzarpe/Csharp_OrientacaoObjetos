591d71893fe7ca41c46e987444128617c96a476c
michel zarpelon <michelzarpe@gmail.com>
2018-06-28 08:46:38 -0300

    Capitulo 1 - Projeto Teste para identificar se a instalação esta funiconando + documentação

---
+++ capitulo 1/TesteSDK/TesteSDK/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteSDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("digite seu nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Seu nome é:"+nome);
+            Console.ReadLine();
+
+
+        }
+    }
+}
