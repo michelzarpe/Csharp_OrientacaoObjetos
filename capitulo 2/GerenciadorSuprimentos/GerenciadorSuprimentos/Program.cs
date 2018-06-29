887acc29f396766060faaf00cdf372adf3d72a6d
michel zarpelon <michelzarpe@gmail.com>
2018-06-29 15:47:04 -0300

    Capitulo 3 - matriz, lista, conjuntos

--- capitulo 2/GerenciadorSuprimentos/GerenciadorSuprimentos/Program.cs
+++ capitulo 2/GerenciadorSuprimentos/GerenciadorSuprimentos/Program.cs
@@ -11,6 +11,7 @@ namespace GerenciadorSuprimentos
     {
         static void Main(string[] args)
         {
+            /*
             Produto pod1 = new Produto("TV", 900.00, 10);
             Console.WriteLine(pod1.ToString());
             pod1.realizarEntrada(5);
@@ -24,6 +25,39 @@ namespace GerenciadorSuprimentos
 
             Console.WriteLine(produtos[0].Nome);
             Console.ReadLine();
+
+    */
+            /*vetorh*/
+            int[] vetor = new int[2];
+
+            /*matriz */
+            int[,] matriz = new int[3, 3];
+
+            /*listas - acessada pela posicao, mas é redimensionada dinamicamente using System.Colletions.Generic*/
+            List<String> lista = new List<String>();
+            lista.Add("NANANAN"); int x=lista.Count; string y = lista[0]; lista.Remove("NANANA"); lista.FindIndex(i => i == "NANANA"); /*se for objeto é i.atributo == "NANANAN" */
+
+            /*Conjunto using System.Colletions.Generic, nao adiciona elementos duplicados*/
+            /**/
+            HashSet<int> ConjuntoA = new HashSet<int>();
+            HashSet<int> ConjuntoB = new HashSet<int>();
+            ConjuntoA.Add(5); ConjuntoA.Add(8); ConjuntoA.Add(9); ConjuntoA.Add(3);
+            ConjuntoB.Add(3); ConjuntoB.Add(4); ConjuntoB.Add(5);
+
+            foreach(int l in ConjuntoA)
+            {
+                Console.WriteLine(l);
+            }
+
+            if (ConjuntoA.Contains(3))
+            {
+                Console.WriteLine("Existe");
+            }
+            else
+            {
+                Console.WriteLine("Não Existe");
+            }
+
         }
     }
 }
