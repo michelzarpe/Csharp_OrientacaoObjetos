06c5403d3979ed361512a4a18bd2265bc1285854
michel zarpelon <michelzarpe@gmail.com>
2018-06-29 14:05:00 -0300

    Capitulo 2 - Classes e Objetos - Gerenciador de Suprimentos

---
+++ capitulo 2/GerenciadorSuprimentos/GerenciadorSuprimentos/Domain/Produto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorSuprimentos.Domain
+{
+    class Produto
+    {
+        private string nome;
+        private double preco;
+        private int quantidadeNoEstoque;
+
+        public Produto(string nome, double preco, int quantidadeNoEstoque)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.QuantidadeNoEstoque = quantidadeNoEstoque;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public double Preco { get => preco; set => preco = value; }
+        public int QuantidadeNoEstoque { get => quantidadeNoEstoque; set => quantidadeNoEstoque = value; }
+
+        public override bool Equals(object obj)
+        {
+            var produto = obj as Produto;
+            return produto != null &&
+                   Nome == produto.Nome &&
+                   Preco == produto.Preco &&
+                   QuantidadeNoEstoque == produto.QuantidadeNoEstoque;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1825794190;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + Preco.GetHashCode();
+            hashCode = hashCode * -1521134295 + QuantidadeNoEstoque.GetHashCode();
+            return hashCode;
+        }
+
+
+        public double valorTotalEmEstoque()
+        {
+            return this.preco * this.quantidadeNoEstoque;
+        }
+
+        public void realizarEntrada(int quantidade)
+        {
+            this.quantidadeNoEstoque += quantidade;
+        }
+
+
+        public void realizarSaida(int quantidade)
+        {
+            this.quantidadeNoEstoque -= quantidade;
+        }
+
+        public string ToString()
+        {
+            return this.nome + ", " +
+                "" + this.preco.ToString("F2",CultureInfo.InvariantCulture) + "R$, " +
+                "" + this.quantidadeNoEstoque+" unidades, valor total: " +
+                ""+ this.valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture) + "R$";
+        }
+
+    }
+}
---
+++ capitulo 2/GerenciadorSuprimentos/GerenciadorSuprimentos/Program.cs
@@ -0,0 +1,24 @@
+﻿using GerenciadorSuprimentos.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorSuprimentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto pod1 = new Produto("TV", 900.00, 10);
+            Console.WriteLine(pod1.ToString());
+            pod1.realizarEntrada(5);
+            Console.WriteLine(pod1.ToString());
+            pod1.realizarSaida(3);
+            Console.WriteLine(pod1.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
