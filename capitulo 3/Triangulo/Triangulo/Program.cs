34b107824bfeb62f1a04d7dc26273db685f1a8ec
michel zarpelon <michelzarpe@gmail.com>
2018-07-02 08:31:57 -0300

    Capitulo 3 - metodos e atributos static

--- capitulo 3/Triangulo/Triangulo/Program.cs
+++ capitulo 3/Triangulo/Triangulo/Program.cs
@@ -11,16 +11,10 @@ namespace Triangulo
     {
         static void Main(string[] args)
         {
-            Figura x = new FigTriangulo(3.00, 4.00, 5.00, "Amarelo","Triangulo");
-            Figura y = new FigTriangulo(7.50, 4.50, 4.02, "Azul","Triangulo");
-
-
-
-            Console.WriteLine(x.Cor + " area -> " + x.getArea());
-            Console.WriteLine(y.Cor + " area -> " + y.getArea());
-
             HashSet<Figura> lista_figuras = new HashSet<Figura>();
 
+
+            Console.WriteLine(Tela.getInicio("Michel Zarpelon"));
             lista_figuras.Add(new FigTriangulo(4.00,4.50,5.00,"Azul","Triangulo"));
             lista_figuras.Add(new Retangulo(12.00,15.00,"Amarelo","Retangulo"));
             lista_figuras.Add(new FigTriangulo(3.00, 4.50, 5.00, "Verde", "Triangulo"));
---
+++ capitulo 3/Triangulo/Triangulo/domain/Tela.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo.domain
+{
+    class Tela
+    {
+
+        public static string getInicio(String msg)
+        {
+            return "Bem Vindo " + msg;
+        }
+    }
+}
